771e202aa53dc8ce86359c8b43e57a1ba0af0c3a
AliceeeSixes <6d6perturn@gmail.com>
2025-01-13 11:28:18 +0000

    add toggle for side nav on medium viewport sizes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,36 @@
 $(".animsition").animsition({
     inClass: "fade-in",
     outClass: "fade-out",
-    linkElement: 'a[href^="nav"]'
-
+    linkElement: 'a',
+    inDuration: 400,
+    outDuration: 600
 });
+
+
+
+
+// Vertical nav toggle
+let sidebar = true;
+let navVerticalWidth = $("#nav__vertical").width();
+console.log(navVerticalWidth);
+function toggleSidebar() {
+    if (sidebar === true) {
+        //hide
+        $("main").css("margin-left","0");
+        $("#nav__vertical").css("left","-100%")
+        $(".nav__vertical--toggle").css("margin-left","0");
+        $(".nav__vertical--toggle button").html(`<i class="fa fa-chevron-right"></i>`);
+        console.log("hidden");
+        sidebar = false;
+    }
+    else {
+        //show
+        $("main").css("margin-left",navVerticalWidth);
+        $("#nav__vertical").css("left","0")
+        $(".nav__vertical--toggle").css("margin-left",navVerticalWidth);   
+        $(".nav__vertical--toggle button").html(`<i class="fa fa-chevron-left"></i>`);
+        console.log("shown");
+        sidebar = true;
+    }
+    
+}
